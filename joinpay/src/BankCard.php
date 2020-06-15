dd1edbd0e9b607ba4bd32792427328de40181e20
夏晓亮 <786689461@qq.com>
2020-06-15 10:53:21 +0800

    声明

--- joinpay/src/BankCard.php
+++ joinpay/src/BankCard.php
@@ -11,6 +11,14 @@ namespace Joinpay;
 
 class BankCard
 {
+    protected $mchOrderNo;
+    protected $payerName;
+    protected $idNo;
+    protected $bankCardNo;
+    protected $mobileNo;
+    protected $expireDate;
+    protected $cvv;
+
     /**
      * 通道绑卡 发送短信
      * @param array $param
@@ -19,16 +27,16 @@ class BankCard
     public function marketChannelAuth(array $param)
     {
         $res =   array(
-            "mch_order_no" => $param['order_no'],
+            "mch_order_no" => $this->mchOrderNo,
             "order_amount" => '0.10',
             "mch_req_time" => date('Y-m-d H:i:s'),
-            "payer_name" => $param['account_name'],// $param['account_name'],
+            "payer_name" => $this->payerName,// $param['account_name'],
             "id_type" => Config::ID_CARD_TYPE,
-            "id_no" => $param['id_card'],//$param['id_card'],
-            "bank_card_no" => $param['bank_number'],//$param['bank_number'],
-            "mobile_no" => $param['mobile'],//$param['mobile'],
-            "expire_date" => $param['effective'],//$param['effective'],
-            "cvv" => $param['cvn2']//$param['cvn2'],
+            "id_no" => $this->idNo,//$param['id_card'],
+            "bank_card_no" => $this->bankCardNo,//$param['bank_number'],
+            "mobile_no" => $this->mobileNo,//$param['mobile'],
+            "expire_date" => $this->expireDate,//$param['effective'],
+            "cvv" => $this->cvv//$param['cvn2'],
         );
 
         $return_data = $this->curl_post($res);
--- joinpay/src/Merchant.php
+++ joinpay/src/Merchant.php
@@ -15,6 +15,10 @@ class Merchant
 {
     protected $altLoginName = '';//登录账号
     protected $altMchName = '';//商户名称
+    protected $legalPerson = '';//姓名
+    protected $phoneNo = '';//手机号
+    protected $idCardNo = '';//身份证号
+    protected $bankAccountNo = '';//银行卡号
 
     /**
      * @return string
@@ -25,31 +29,130 @@ class Merchant
     }
 
     /**
-     * @param string $altLoginName
+     * @param $altLoginName
+     * @return $this
      */
     public function setAltLoginName($altLoginName)
     {
         $this->altLoginName = $altLoginName;
+        return $this;
     }
+
+    /**
+     * @return string
+     */
+    protected function getAltMchName()
+    {
+        return $this->altMchName;
+
+    }
+
+    /**
+     * @param $altMchName
+     * @return $this
+     */
+    public function setAltMchName($altMchName)
+    {
+        $this->altMchName = $altMchName;
+        return $this;
+
+    }
+
+    /**
+     * @return string
+     */
+    protected function getBankAccountNo()
+    {
+        return $this->bankAccountNo;
+    }
+
+    /**
+     * @param $bank_account_no
+     * @return $this
+     */
+    public function setBankAccountNo($bank_account_no)
+    {
+        $this->bankAccountNo = $bank_account_no;
+        return $this;
+
+    }
+
+    /**
+     * @return string
+     */
+    protected function getIdCardNo()
+    {
+        return $this->idCardNo;
+    }
+
+    /**
+     * @param $id_card_no
+     * @return $this
+     */
+    public function setIdCardNo($id_card_no)
+    {
+        $this->idCardNo = $id_card_no;
+        return $this;
+
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLegalPerson()
+    {
+        return $this->legalPerson;
+    }
+
+    /**
+     * @param $legal_person
+     * @return $this
+     */
+    public function setLegalPerson($legal_person)
+    {
+        $this->legalPerson = $legal_person;
+        return $this;
+
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPhoneNo()
+    {
+        return $this->phoneNo;
+    }
+
+    /**
+     * @param $phone_no
+     * @return $this
+     */
+    public function setPhoneNo($phone_no)
+    {
+        $this->phoneNo = $phone_no;
+        return $this;
+
+    }
+
     /**
      * 添加商户
-     * @param array $param
      * @param array $message
      * @return bool|string
      * @throws \Exception
      */
-    public function addMerchant($param = array(),&$message = array()){
+    public function addMerchant(&$message = array())
+    {
 
         $merch = array(
-            'alt_login_name' => $param['alt_login_name'],//'123456',//登录账号
-            'alt_mch_name' => $param['alt_mch_name'], //'个体户李四',//商户名称
-            'legal_person' =>  $param['legal_person'],//'李四',//姓名
-            'phone_no' =>  $param['phone_no'],//'1998888777',//手机号
-            'id_card_no' => $param['id_card_no'],// '100101199410104784',//身份证
-            'bank_account_no' => $param['bank_account_no'],// '6222020000000456',//银行卡（储蓄）
+            'alt_login_name' => $this->altLoginName,//'123456',//登录账号
+            'alt_mch_name' => $this->altMchName, //'个体户李四',//商户名称
+            'legal_person' => $this->legalPerson,//'李四',//姓名
+            'phone_no' => $this->phoneNo,//'1998888777',//手机号
+            'id_card_no' => $this->idCardNo,// '100101199410104784',//身份证
+            'bank_account_no' => $this->bankAccountNo,// '6222020000000456',//银行卡（储蓄）
             'callback_url' => Config::MERGHANT_NOTIFY,
         );
-        $red = RequestUtil::curl_post($merch,$message);
+        $red = RequestUtil::curl_post($merch, $message);
 
         return $red;
     }
