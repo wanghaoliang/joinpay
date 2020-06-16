8bbeb11a9c6aba92a7326ebc0a554947dfb5a7d8
Mely <786689461@qq.com>
2020-06-16 09:55:10 +0800

    //修改添加卡

--- joinpay/src/BankCard.php
+++ joinpay/src/BankCard.php
@@ -9,6 +9,8 @@
 namespace Joinpay;
 
 
+use Joinpay\util\RequestUtil;
+
 class BankCard
 {
     protected $mchOrderNo;
@@ -20,11 +22,143 @@ class BankCard
     protected $cvv;
 
     /**
-     * 通道绑卡 发送短信
-     * @param array $param
-     * @return bool|mixed|string
+     * @param $mchOrderNo
+     * @return $this
      */
-    public function marketChannelAuth(array $param)
+    public function setMchOrderNo($mchOrderNo)
+    {
+        $this->mchOrderNo = $mchOrderNo;
+        return $this;
+    }
+
+    /**
+     * @param mixed $bankCardNo
+     * @return BankCard
+     */
+    public function setBankCardNo($bankCardNo)
+    {
+        $this->bankCardNo = $bankCardNo;
+        return $this;
+    }
+
+    /**
+     * @param mixed $payerName
+     * @return BankCard
+     */
+    public function setPayerName($payerName)
+    {
+        $this->payerName = $payerName;
+        return $this;
+    }
+
+    /**
+     * @param $cvv
+     * @return $this
+     */
+    public function setCvv($cvv)
+    {
+        $this->cvv = $cvv;
+        return $this;
+
+    }
+
+    /**
+     * @param $expireDate
+     * @return $this
+     */
+    public function setExpireDate($expireDate)
+    {
+        $this->expireDate = $expireDate;
+        return $this;
+
+    }
+
+    /**
+     * @param $idNo
+     * @return $this
+     */
+    public function setIdNo($idNo)
+    {
+        $this->idNo = $idNo;
+        return $this;
+
+    }
+
+    /**
+     * @param $mobileNo
+     * @return $this
+     */
+    public function setMobileNo($mobileNo)
+    {
+        $this->mobileNo = $mobileNo;
+        return $this;
+
+    }
+
+
+    /**
+     * @return mixed
+     */
+    protected function getBankCardNo()
+    {
+        return $this->bankCardNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getCvv()
+    {
+        return $this->cvv;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getExpireDate()
+    {
+        return $this->expireDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getIdNo()
+    {
+        return $this->idNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getMchOrderNo()
+    {
+        return $this->mchOrderNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getMobileNo()
+    {
+        return $this->mobileNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getPayerName()
+    {
+        return $this->payerName;
+    }
+
+
+    /**
+     * @param array $message
+     * @return bool|string
+     * @throws \Exception
+     */
+    public function marketChannelAuth(&$message = array())
     {
         $res =   array(
             "mch_order_no" => $this->mchOrderNo,
@@ -39,7 +173,7 @@ class BankCard
             "cvv" => $this->cvv//$param['cvn2'],
         );
 
-        $return_data = $this->curl_post($res);
+        $return_data =RequestUtil::curl_post($res,&$message);
         return $return_data;
     }
 }
